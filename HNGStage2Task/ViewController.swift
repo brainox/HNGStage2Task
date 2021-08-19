f340ce601b5be3f4736f8b977606a5790bba5ac7
brainox <macmartins081@gmail.com>
2021-08-19 15:29:40 +0100

    Initial Commit

---
+++ HNGStage2Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HNGStage2Task
+//
+//  Created by Decagon on 19/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
